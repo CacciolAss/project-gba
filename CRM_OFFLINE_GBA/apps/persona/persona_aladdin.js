a607da80705442f49b99302cfdd69318823577ff
CacciolAss <CacciolAss281@gmail.com>
2026-02-02 15:36:08 +0100

    Update persona_aladdin.js
    feat: aggiunto salvataggio stato familiare (A2)

--- CRM_OFFLINE_GBA/apps/persona/persona_aladdin.js
+++ CRM_OFFLINE_GBA/apps/persona/persona_aladdin.js
@@ -17,6 +17,24 @@ const Aladdin = {
     
     console.log("💰 Reddito salvato:", importoReddito);
     return appState;
+  },
+  
+  // Funzione che prende la risposta A2 (stato familiare) e la mette nel contesto  
+  salvaStatoFamiliare: function(appState, statoFamiliare) {
+    // Controlla se esiste già il cassetto "fatti"
+    if (!appState.fattiCliente) {
+      appState.fattiCliente = {};
+    }
+    
+    // Mettiamo lo stato familiare nel cassetto giusto
+    appState.fattiCliente.statoFamiliare = {
+      valore: statoFamiliare,
+      quando: new Date().toLocaleDateString('it-IT'),
+      domanda: "A2"
+    };
+    
+    console.log("👨‍👩‍👧‍👦 Stato familiare salvato:", statoFamiliare);
+    return appState;
   }
   
 };
